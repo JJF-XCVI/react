22e3ca22f8958069c6ee985dfc95dc8c9d86fdc7
Jere Fetula <jf8726@edu.turku.fi>
2026-08-13 12:52:12 +0300

    12:52

--- harjoitus5.js
+++ harjoitus5.js
@@ -1,7 +1,8 @@
+
 function odota(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
-
+//sain kaiken koodista tekstistä Akronisuus vaihdoin vaa tervehdi viivellä laske nimi a,b sitten annoin arvot a ja b
 async function laskeViivella(a,b) {
     await odota(1000);
     return a + b;
